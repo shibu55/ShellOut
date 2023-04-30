b5bc9923b12752655c31f816a5d5093c33013070
shibu55 <segusuigao@gmail.com>
2023-04-30 13:33:31 +0900

    wave

--- js/wave.js
+++ js/wave.js
@@ -1,33 +1,38 @@
 $(document).ready(function() {
   const canvas = document.getElementById("waveCanvas");
   const ctx = canvas.getContext("2d");
-  
+
   const devicePixelRatio = window.devicePixelRatio || 1;
-  
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  
+
+  canvas.width = window.innerWidth * devicePixelRatio;
+  canvas.height = window.innerHeight * devicePixelRatio;
   canvas.style.width = window.innerWidth + "px";
   canvas.style.height = window.innerHeight + "px";
-  
-  ctx.scale(devicePixelRatio, devicePixelRatio);
+
+  if (window.innerWidth <= 480) {
+    canvas.width /= 2;
+    canvas.height /= 2;
+    // ctx.scale(0.5, 0.5);
+  } else {
+    // ctx.scale(devicePixelRatio, devicePixelRatio);
+  }
 
   const waves = [
     {
       // yellow
-      y: canvas.height * 1/devicePixelRatio - 40,
-      length: 0.005,
+      y: canvas.height *0.9,
+      length: 0.01,
       amplitude: 30,
-      frequency: 0.05,
-      color: "rgba(255,248,46, 0.5)"
+      frequency: 0.01,
+      color: "rgba(255,248,46, 1)"
     },
     {
       // orange
-      y: canvas.height * 1/devicePixelRatio - 50,
-      length: 0.007,
+      y: canvas.height *0.88,
+      length: 0.015,
       amplitude: 25,
-      frequency: 0.1,
-      color: "rgba(255,157,46, 0.5)"
+      frequency: 0.015,
+      color: "rgba(255,157,46, 1)"
     }
   ];
 
@@ -37,7 +42,7 @@ $(document).ready(function() {
     ctx.beginPath();
     ctx.moveTo(0, wave.y);
 
-    for (let x = 0; x < canvas.width; x+=0.5) {
+    for (let x = 0; x < canvas.width; x++) {
       const y =
         wave.amplitude * Math.sin(x * wave.length + wave.frequency) + wave.y;
       ctx.lineTo(x, y);
