9d5a9a9e1521258866fec2bcd87148f4e82437e7
shibu55 <segusuigao@gmail.com>
2023-05-10 22:43:30 +0900

    paragraph

---
+++ js/paragraph.js
@@ -0,0 +1,159 @@
+$(document).ready(function () {
+  const text = `完全新規アイドルグループ「My Plustar(マイプラスター)」初期メンバー募集中！<br>
+  マイナスな自分から、プラスに輝くスターへ<br>
+  <br>
+  アイドルになりたい…！漠然と興味はあるけれど、アクションが起こせなかった、気付いたら応募締め切りを過ぎていた、自信が持てなかった…<br>
+  消極的な自分とは別れを告げ、今ここで新たな一歩を踏み出し、私たちと一緒にステージで夢を叶えませんか？<br>
+
+
+  歌やダンスは完全未経験でOK！My Plustarは0からのスタートを応援します少しずつ上達しながら実力を身につけ、デビュー半年を目処にワンマンライブ、やがては全国ツアーを開催できるグループを目指します<br>
+  <br>
+  ★応募資格★<br>
+  ・16〜29歳までの方・特定のプロダクション、レコード会社、その他と契約がない方・東京都内の活動に支障がない方・心身ともに健康な方・決められた時間やルールを守れる方・主体性を持って活動できる方<br>
+
+
+  ★応募方法★<br>
+  “メールアドレス未定”件名を「My Plustarオーディション応募」として必要事項を記載/添付して送信<br>
+  ・名前（本名）・生年月日・身長/体重・お住まいの都道府県・正面から撮影した写真を全身1枚＋上半身1枚（加工していない写真で、全身写真はスタイルがわかるもの）・現在使用しているSNSアカウント(Twitter、Instagram、TikTok)・芸能活動歴の有無（有る場合は具体的に記入をお願いします）・志望動機・自己PR(方法は指定しません、あなたの強みを見せてください)<br>
+
+
+  ★オーディションのスケジュール★<br>
+  【1次】書類選考到着順に審査します、通過者のみ到着後3日以内に連絡いたします<br>
+  ↓<br>
+  【2次】面接詳細は1次審査通過者のみ連絡いたします<br>
+  ↓<br>
+  【3次】歌唱力・ダンス<br>
+  詳細は2次審査通過者のみ連絡いたします<br>
+
+
+  ★合格後のスケジュール★<br>
+  レッスン開始、楽曲を4曲習得していただきます(同時並行で衣装採寸、アーティスト写真撮影などデビューに向けての準備）<br>
+  ↓<br>
+  オンラインにてプレデビュー<br>
+  ↓<br>
+  2023年○月ステージデビュー(メンバーの成長具合によって前後することがあります)<br>
+
+
+  ★オーディション参加費★<br>
+  面接会場までの交通費のみ負担していただきます<br>
+  ★合格後にかかる費用★<br>
+  衣装代、レッスン費など活動にかかる費用は一切頂きませんレッスン・ライブ会場への交通費のみ自己負担となります<br>
+  
+  ★給与、手当など★<br>
+  所定の報酬をお支払いします。`;
+  const mobile_text = `完全新規アイドルグループ「My Plustar(マイプラスター)」初期メンバー募集中！<br>
+  マイナスな自分から、プラスに輝くスターへ<br>
+  <br>
+  アイドルになりたい…！漠然と興味はあるけれど、アクションが起こせなかった、気付いたら応募締め切りを過ぎていた、自信が持てなかった…<br>
+  消極的な自分とは別れを告げ、今ここで新たな一歩を踏み出し、私たちと一緒にステージで夢を叶えませんか？<br>
+
+
+  歌やダンスは完全未経験でOK！My Plustarは0からのスタートを応援します少しずつ上達しながら実力を身につけ、デビュー半年を目処にワンマンライブ、やがては全国ツアーを開催できるグループを目指します<br>
+  <br>
+  ★応募資格★<br>
+  ・16〜29歳までの方・特定のプロダクション、レコード会社、その他と契約がない方・東京都内の活動に支障がない方・心身ともに健康な方・決められた時間やルールを守れる方・主体性を持って活動できる方<br>
+
+
+  ★応募方法★<br>
+  “メールアドレス未定”件名を「My Plustarオーディション応募」として必要事項を記載/添付して送信<br>
+  ・名前（本名）・生年月日・身長/体重・お住まいの都道府県・正面から撮影した写真を全身1枚＋上半身1枚（加工していない写真で、全身写真はスタイルがわかるもの）・現在使用しているSNSアカウント(Twitter、Instagram、TikTok)・芸能活動歴の有無（有る場合は具体的に記入をお願いします）・志望動機・自己PR(方法は指定しません、あなたの強みを見せてください)<br>
+
+
+  ★オーディションのスケジュール★<br>
+  【1次】書類選考到着順に審査します、通過者のみ到着後3日以内に連絡いたします<br>
+  ↓<br>
+  【2次】面接詳細は1次審査通過者のみ連絡いたします<br>
+  ↓<br>
+  【3次】歌唱力・ダンス<br>
+  詳細は2次審査通過者のみ連絡いたします<br>
+
+
+  ★合格後のスケジュール★<br>
+  レッスン開始、楽曲を4曲習得していただきます(同時並行で衣装採寸、アーティスト写真撮影などデビューに向けての準備）<br>
+  ↓<br>
+  オンラインにてプレデビュー<br>
+  ↓<br>
+  2023年○月ステージデビュー(メンバーの成長具合によって前後することがあります)<br>
+
+
+  ★オーディション参加費★<br>
+  面接会場までの交通費のみ負担していただきます<br>
+  <br>
+  ★合格後にかかる費用★<br>
+  衣装代、レッスン費など活動にかかる費用は一切頂きませんレッスン・ライブ会場への交通費のみ自己負担となります<br>
+  <br>
+  ★給与、手当など★<br>
+  所定の報酬をお支払いします。`;
+
+  const contents = text.split('\n\n\n');
+  const mobile_contents = mobile_text.split('\n\n\n');
+
+  let currentIndex = 0;
+
+  function updateContent() {
+    $('.non-recruit-description').html(contents[currentIndex] + '<br/>');
+    updateButtonsVisibility();
+    appendButtons();
+  }
+
+  function addButton(buttonClass, buttonText) {
+    const button = $('<button></button>', {
+      class: buttonClass,
+      text: buttonText,
+    });
+    button.css({
+      display: 'inline-block',
+      marginTop: '30px',
+      padding: '10px 20px',
+      backgroundColor: '#683a1b',
+      color: '#ffffff',
+      fontSize: '16px',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      borderRadius: '5px',
+      cursor: 'pointer',
+    });
+    return button;
+  }
+
+  const prevButton = addButton('prev-button', '前へ');
+  const nextButton = addButton('next-button', '次へ');
+
+  function updateButtonsVisibility() {
+    if (currentIndex === 0) {
+      prevButton.hide();
+      nextButton.show();
+      nextButton.css({ marginLeft: '0' });
+    } else if (currentIndex === contents.length - 1) {
+      prevButton.show();
+      nextButton.hide();
+      prevButton.css({ marginRight: '0' });
+    } else {
+      prevButton.show();
+      nextButton.show();
+      nextButton.css({ marginLeft: '10px' });
+    }
+  }
+
+  function appendButtons() {
+    $('.non-recruit-description').append(prevButton, nextButton);
+
+    $('.prev-button').on('click', function () {
+      currentIndex--;
+      if (currentIndex < 0) {
+        currentIndex = contents.length - 1;
+      }
+      updateContent();
+    });
+
+    $('.next-button').on('click', function () {
+      currentIndex++;
+      if (currentIndex >= contents.length) {
+        currentIndex = 0;
+      }
+      updateContent();
+    });
+  }
+
+  updateContent();
+});
