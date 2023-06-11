36e1d15d6ceee7c4cd4c3018aaf6142b610bd920
shibu55 <segusuigao@gmail.com>
2023-06-11 13:38:19 +0900

    recruit description

--- js/paragraph.js
+++ js/paragraph.js
@@ -2,25 +2,39 @@ $(document).ready(function () {
   const text = `完全新規アイドルグループ「My Plustar(マイプラスター)」初期メンバー募集中！<br>
   マイナスな自分から、プラスに輝くスターへ<br>
   <br>
-  アイドルになりたい…！漠然と興味はあるけれど、アクションが起こせなかった、気付いたら応募締め切りを過ぎていた、自信が持てなかった…<br>
-  消極的な自分とは別れを告げ、今ここで新たな一歩を踏み出し、私たちと一緒にステージで夢を叶えませんか？<br>
-
-
-  歌やダンスは完全未経験でOK！<br>My Plustarは0からのスタートを応援します<br>少しずつ上達しながら実力を身につけ、デビュー半年を目処にワンマンライブ、やがては全国ツアーを開催できるグループを目指します<br>
+  アイドルをやってみたい…興味はあるけれど、自信を持てなくてアクションが起こせなかったあなた、今ここで新たな一歩を踏み出し、私たちと一緒にステージで夢を叶えませんか？<br>
   <br>
+  歌やダンスは完全未経験でOK！<br>
+  My Plustarは0からのスタートを応援します<br>
+  少しずつ上達しながら実力を身につけ、デビュー半年を目処にワンマンライブ、やがては全国ツアーを開催できるグループを目指します<br>
+
+
   ★応募資格★<br>
-  ・16〜29歳までの方・特定のプロダクション、レコード会社、その他と契約がない方・東京都内の活動に支障がない方・心身ともに健康な方・決められた時間やルールを守れる方・主体性を持って活動できる方<br>
+  ・16〜29歳までの方<br>
+  ・東京都内の活動に支障がない方<br>
+  ・心身ともに健康な方<br>
+  ・決められた時間やルールを守れる方<br>
+  ・主体性を持って活動できる方<br>
+  ・特定のプロダクション、レコード会社、その他と契約がない方<br>
 
 
   ★応募方法★<br>
-  “メールアドレス未定”件名を「My Plustarオーディション応募」として必要事項を記載/添付して送信<br>
-  ・名前（本名）・生年月日・身長/体重・お住まいの都道府県・正面から撮影した写真を全身1枚＋上半身1枚（加工していない写真で、全身写真はスタイルがわかるもの）・現在使用しているSNSアカウント(Twitter、Instagram、TikTok)・芸能活動歴の有無（有る場合は具体的に記入をお願いします）・志望動機・自己PR(方法は指定しません、あなたの強みを見せてください)<br>
+  ・“メールアドレス”又は応募用公式LINEに必要事項を記載/添付して送信、メールの場合は件名に「My Plustarオーディション応募」と記入<br>
+  ・名前（本名） 生年月日 身長/体重<br>
+  ・お住まいの都道府県<br>
+  ・正面から撮影した写真を全身1枚＋上半身1枚（加工していない写真で、全身写真はスタイルがわかるもの）<br>
+  ・現在使用しているSNSアカウント(Twitter、Instagram、TikTok)<br>
+  ・芸能活動歴の有無（有る場合は具体的に記入をお願いします）<br>
+  ・志望動機・自己PR(方法は指定しません、あなたの強みを見せてください)<br>
 
 
   ★オーディションのスケジュール★<br>
-  【1次】書類選考到着順に審査します、通過者のみ到着後3日以内に連絡いたします<br>
+  【1次】書類選考<br>
+  到着順に審査します<br>
+  通過者のみ到着後3日以内に連絡いたします<br>
   ↓<br>
-  【2次】面接詳細は1次審査通過者のみ連絡いたします<br>
+  【2次】面接<br>
+  詳細は1次審査通過者のみ連絡いたします<br>
   ↓<br>
   【3次】歌唱力・ダンス<br>
   詳細は2次審査通過者のみ連絡いたします<br>
@@ -31,16 +45,18 @@ $(document).ready(function () {
   ↓<br>
   オンラインにてプレデビュー<br>
   ↓<br>
-  2023年○月ステージデビュー(メンバーの成長具合によって前後することがあります)<br>
+  2023年10月〜11月にステージデビュー<br>
+  (メンバーの成長具合によって前後することがあります)<br>
 
 
-  ★オーディション参加費★<br>
+  ・オーディション参加費<br>
   面接会場までの交通費のみ負担していただきます<br>
-  ★合格後にかかる費用★<br>
+  <br>
+  ・合格後にかかる費用<br>
   衣装代、レッスン費など活動にかかる費用は一切頂きませんレッスン・ライブ会場への交通費のみ自己負担となります<br>
-  
-  ★給与、手当など★<br>
-  所定の報酬をお支払いします。`;
+  <br>
+  ・給与、手当など<br>
+  所定の報酬をお支払いします。<br>`;
   const mobile_text = `完全新規アイドルグループ「My Plustar(マイプラスター)」初期メンバー募集中！<br>
   マイナスな自分から、プラスに輝くスターへ<br>
   <br>
@@ -91,7 +107,8 @@ $(document).ready(function () {
   let currentIndex = 0;
 
   function updateContent() {
-    $('.non-recruit-description').html(contents[currentIndex] + '<br/>');
+    const content = contents[currentIndex];
+    $('.recruit-description').hide().html(content).fadeIn(500);
     updateButtonsVisibility();
     appendButtons();
   }
@@ -100,18 +117,6 @@ $(document).ready(function () {
     const button = $('<button></button>', {
       class: buttonClass,
       text: buttonText,
-    });
-    button.css({
-      display: 'inline-block',
-      marginTop: '30px',
-      padding: '10px 20px',
-      backgroundColor: '#683a1b',
-      color: '#ffffff',
-      fontSize: '16px',
-      fontWeight: 'bold',
-      textAlign: 'center',
-      borderRadius: '5px',
-      cursor: 'pointer',
     });
     return button;
   }
@@ -136,7 +141,10 @@ $(document).ready(function () {
   }
 
   function appendButtons() {
-    $('.non-recruit-description').append(prevButton, nextButton);
+    const buttonWrapper = $('<div></div>');
+    buttonWrapper.addClass('button-wrapper');
+    buttonWrapper.append(prevButton, nextButton);
+    $('.recruit-description').append(buttonWrapper);
 
     $('.prev-button').on('click', function () {
       currentIndex--;
---
+++ js/test.js
@@ -0,0 +1,147 @@
+$(function () {
+  var MENU_NAMES = ["about", "office", "recruit", "contact"];
+  var NENU_CLASSES = MENU_NAMES.map(function (name) {
+    return "." + name;
+  });
+  var FADE_DURATION = 250;
+  var cancelFlag = 0;
+
+  function showContent(name, duration) {
+    $(".index, .content").fadeOut(duration);
+    setTimeout(function () {
+      $(NENU_CLASSES.join(",")).hide();
+      $(".content").scrollTop();
+      $(".content").addClass("menu-content");
+      var names = [".header-logo", ".footer-list", ".menu-content", "." + name];
+      $(names.join(",")).fadeIn(duration).show();
+    }, duration);
+  }
+
+  function showTop(duration) {
+    $(".header-logo, .footer-list, .content").fadeOut(duration);
+    setTimeout(function () {
+      $(NENU_CLASSES.concat([".menu-content"]).join(",")).hide();
+      $(".content").removeClass("menu-content");
+      $(".content, .index").fadeIn(duration);
+    }, duration);
+  }
+
+  function navigate(name, fromBrowser = false, initial = false) {
+    var currentName = window.location.pathname.substring(1);
+    if (
+      !fromBrowser &&
+      !initial &&
+      ((name === "index" && currentName === "") || name === currentName)
+    )
+    return;
+    if (cancelFlag === 1) return;
+    cancelFlag = 1;
+    if (!fromBrowser) {
+      var path = name === "index" ? "/" : "/" + name;
+      history.pushState({ name: name }, null, path);
+    }
+    var duration = initial ? 0 : FADE_DURATION;
+    if (name === "index") {
+      showTop(duration);
+      $(".link-underline").removeClass("active");
+    } else {
+      showContent(name, duration);
+      $(".link-underline").removeClass("active");
+      $(".menu-" + name + " .link-underline").addClass("active");
+    }
+    setTimeout(function () {
+      cancelFlag = 0;
+    }, duration * 2);
+  }
+
+  window.submitCheck = function () {
+    if ($(".contact-confirm-dialog").is(":visible")) {
+      $(".contact-confirm-dialog").fadeOut(FADE_DURATION);
+      $("form[name='contact-form']").submit();
+    } else {
+      $('[data-confirm-name="name"]').text($('[data-name="name"]').val());
+      $('[data-confirm-name="email"]').text($('[data-name="email"]').val());
+      $('[data-confirm-name="tel"]').text($('[data-name="tel"]').val());
+      $('[data-confirm-name="company"]').text($('[data-name="company"]').val());
+      $('[data-confirm-name="inquiry"]').html(
+        $('[data-name="inquiry"]').val().replace(/\r?\n/g, "<br>")
+      );
+      $(".contact-confirm-dialog").fadeIn(FADE_DURATION);
+    }
+  };
+
+  MENU_NAMES.forEach(function (name) {
+    $(".menu-" + name).on("click", function () {
+      navigate(name);
+    });
+  });
+
+  $(".close, .header-logo, .close-area").on("click", function () {
+    navigate("index");
+  });
+
+  window.addEventListener("popstate", function (event) {
+    navigate(event.state ? event.state.name : "index", true);
+  });
+
+  var name = window.location.pathname.substring(1);
+  if (name) {
+    if (MENU_NAMES.includes(name)) {
+      navigate(name, false, true);
+    } else if (name === "contact_complete") {
+      alert(
+        "お問合せ内容を送信しました。\n後ほど、担当者よりご連絡をさせていただきます。"
+      );
+      history.replaceState({ name: "contact" }, null, "/contact");
+      window.location.pathname = "/contact";
+    } else {
+      window.location = "/";
+    }
+  } else {
+    navigate("index", false, true);
+  }
+
+  $(".contact-confirm-dialog").on("click", function (e) {
+    if (e.target === this) {
+      $(".contact-confirm-dialog").fadeOut(FADE_DURATION);
+    }
+  });
+  $(".contact-confirm-dialog").hide();
+  });
+
+$(function () {
+  async function webStorage() {
+    // if (sessionStorage.getItem('access')) {
+    //   $(".loading").addClass('is-active');
+      await fadeInOutPhilosophy();
+    // } else {
+      // sessionStorage.setItem('access', 'true');
+      // $(".loading-animation").addClass('is-active');
+      // await delay(3000);
+      // $(".loading").addClass('is-active');
+      // $(".loading-animation").removeClass('is-active');
+    //   await fadeInOutPhilosophy();
+    // }
+  }
+
+  // function delay(ms) {
+  //   return new Promise(resolve => setTimeout(resolve, ms));
+  // }
+
+  function fadeInOutPhilosophy() {
+    return new Promise(resolve => {
+      $(".philosophy-h").fadeIn(2000, function () {
+        $(".philosophy-p").fadeIn(2000, function () {
+          $(".corporate-philosophy").fadeOut(2000, function () {
+            $(".logo-wrap").css("display", "block");
+            $(".logo-wrap").animate({ opacity: 1 }, 2000, function () {
+              resolve();
+            });
+          });
+        });
+      });
+    });
+  }
+  // $(".contact-confirm-dialog").hide();
+  webStorage();
+});
